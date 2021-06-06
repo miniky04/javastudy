68ea9a7869bfb4ca09cf61631f1064ac1ad1d31a
miniky04 <miniky04@naver.com>
2021-06-06 23:38:08 +0900

    BOJ math bronze5 reverse1439a_multiply_b_10998 solve upload

---
+++ src/me/mang00/algorithm/baekjoon/a_multiply_b_10998.java
@@ -0,0 +1,19 @@
+package me.mang00.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class a_multiply_b_10998 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(0 < a && b < 10){
+            int c = a * b;
+            System.out.println(c);
+        }
+    }
+}
