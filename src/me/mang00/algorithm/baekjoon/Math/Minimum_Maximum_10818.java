f014de451ab50b7896998791ba873d97b79e2089
miniky04 <miniky04@naver.com>
2021-06-11 21:12:27 +0900

    BOJ math bronze3 Minimum_Maximum_10818 solve upload

---
+++ src/me/mang00/algorithm/baekjoon/Math/Minimum_Maximum_10818.java
@@ -0,0 +1,20 @@
+package me.mang00.algorithm.baekjoon.Math;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Minimum_Maximum_10818 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[sc.nextInt()];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[arr.length - 1]);
+    }
+}
