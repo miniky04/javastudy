e41831e7131fbccb4d42af85147bf3026c313ddf
miniky04 <miniky04@naver.com>
2021-06-12 19:30:59 +0900

    BOJ math bronze5 Remainder_10430 solve upload

---
+++ src/me/mang00/algorithm/baekjoon/Math/Remainder_10430.java
@@ -0,0 +1,22 @@
+package me.mang00.algorithm.baekjoon.Math;
+
+import java.util.*;
+import java.io.*;
+
+public class Remainder_10430 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c) + (b%c)) % c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c) * (b%c)) % c);
+    }
+}
