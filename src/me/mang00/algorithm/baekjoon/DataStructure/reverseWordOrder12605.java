5ccf9e89a89b49b5ef69463b22ede21ea17b89a7
miniky04 <miniky04@naver.com>
2021-06-18 22:29:34 +0900

    BOJ Stack bronze1 reverseWordOrder12605 solve upload

---
+++ src/me/mang00/algorithm/baekjoon/DataStructure/reverseWordOrder12605.java
@@ -0,0 +1,31 @@
+package me.mang00.algorithm.baekjoon.DataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class reverseWordOrder12605 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            StringBuilder sb = new StringBuilder();
+
+            String str = br.readLine();
+            List<String> reverseList = Arrays.asList(str.split(" "));
+
+            Collections.reverse(reverseList);
+
+            for (String reverseStr : reverseList) {
+                sb.append(reverseStr).append(" ");
+            }
+            System.out.println("Case #" + (i + 1) + ": " + sb);
+        }
+    }
+}
